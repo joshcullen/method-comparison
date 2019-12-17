12f22892da8e96a58e62179fbe66f429b2c2f6e3
Josh Cullen <joshcullen@Joshs-MacBook-Pro-3.local>
2019-12-17 17:05:17 -0600

    Add code for simulating a BCRW and CVM model to test ACs and behavior classification. Also generate KDE and compare against ACs from snail kites

---
+++ Compare KDE vs ACs.R
@@ -0,0 +1,63 @@
+#Compare KDE against modeled ACs
+
+library(tidyverse)
+library(adehabitatHR)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(viridis)
+
+
+dat<- read.csv("Snail Kite Gridded Data_larger.csv", as.is = TRUE)
+
+
+###################
+### Modeled ACs ###
+###################
+
+ac.coords<- read.csv("Activity Center Coordinates.csv", header = T, sep = ',')
+
+## Map
+
+#Load world map data
+usa <- ne_states(country = "United States of America", returnclass = "sf")
+fl<- usa %>% filter(name == "Florida")
+fl<- sf::st_transform(fl, crs = "+init=epsg:32617") #change projection to UTM 17N
+
+nests<- dat %>% group_by(id) %>% dplyr::select(c(id, x, y)) %>% slice(n=1)
+
+
+# ACs
+ggplot() +
+  geom_sf(data = fl) +
+  coord_sf(xlim = c(min(dat$x-20000), max(dat$x+20000)),
+           ylim = c(min(dat$y-20000), max(dat$y+20000)), expand = FALSE) +
+  geom_point(data = dat, aes(x, y, color = "Raw"), size = 1, alpha = 0.5) +
+  geom_point(data = ac.coords, aes(x, y, color = "Model"), size = 3) +
+  geom_point(data = nests, aes(x, y, color = "Nests"), shape = 17, size = 2) +
+  labs(x="Longitude", y="Latitude") +
+  scale_color_manual("", values = c(viridis(n=5)[3],"red","grey45")) +
+  guides(color = guide_legend(override.aes = list(shape = c(16,17,16)))) +
+  theme_bw()
+
+
+
+
+###########
+### KDE ###
+###########
+
+dat.spdf<- dat[,c("x","y")]
+coordinates(dat.spdf)<- ~x+y
+proj4string(dat.spdf)<- CRS("+init=epsg:32617")
+
+# 5 km w 1 cell buffer
+grid<- raster(extent(dat.spdf) + 10000)
+res(grid)<- 500
+proj4string(grid)<- CRS("+init=epsg:32617")
+grid[]<- 0
+
+grid.spxdf<- as(grid, 'SpatialPixels')
+
+kde.href<- kernelUD(dat.spdf, h="href", kern = "bivnorm", grid = grid.spxdf)
+image(kde.href)
+points(ac.coords[,1:2], pch=16, cex=1)
---
+++ Simulated Tracks.R
@@ -0,0 +1,135 @@
+### Create simulated data with activity centers and multiple behaviors as defined by differences in step length and turning angle
+
+
+library(waddle)
+library(tidyverse)
+library(viridis)
+
+### My Simulation: Only for identifying ACs since not enough control over SL and TA ###
+## Uses multistate BCRW model
+set.seed(1)
+
+#multistate BCRW w 5 phases
+ns <- c(100,50,100,50,100)
+Z.centers <- c(0,10-5i,10-5i,30,30)
+attractions <- c(0.9,0.9,0.9,0.9,0.9)
+rhos <- c(0.2,0.2,0.2,0.9,0.2)
+# a <- c(5,1,5,1,5)
+# b <- c(1,2,1,10,1)
+BCRW.sim <- multiBCRW(rhos=rhos, attractions=attractions, Z.centers=Z.centers, ns=ns)
+plot(BCRW.sim, col = viridis(n=3)[c(1,2,1,3,1)])
+points(Z.centers, cex=3, col="orange", lwd=2)
+
+#output is list of vectors for coords, behaviors, and duration of each behavior
+
+# step length
+S <- diff(BCRW.sim$Z)
+
+nu.means<- matrix(NA, 5, 1)
+for (i in 1:length(unique(BCRW.sim$Phase))) {
+  ind<- which(BCRW.sim$Phase == i)
+  
+  nu.means[i,]<- mean(BCRW.sim$Z[ind] %>% diff() %>% Mod(), na.rm = T)
+}
+
+plot.ts(Mod(S))
+abline(v = c(100,150,250,300), lwd=3, col="darkgrey", lty=3)
+clip(1,100,0,20)
+abline(h=nu.means[1], col="red", lwd=2)
+clip(101,150,0,20)
+abline(h=nu.means[2], col="red", lwd=2)
+clip(151,250,0,20)
+abline(h=nu.means[3], col="red", lwd=2)
+clip(251,300,0,20)
+abline(h=nu.means[4], col="red", lwd=2)
+clip(300,400,0,20)
+abline(h=nu.means[5], col="red", lwd=2)
+
+# absolute orientation
+Phi <- Arg(diff(BCRW.sim$Z))
+# turning angles
+Theta <- diff(Phi)
+# rose diagrams
+require(circular)
+par(mfrow=c(1,5))
+rose.diag(Theta[1:100], bins=8, main="Phase I", prop=2, col=viridis(n=3)[1])
+rose.diag(Theta[101:150],bins=8, main="Phase II", prop=2, col=viridis(n=3)[2])
+rose.diag(Theta[151:250], bins=8, main="Phase III", prop=2, col=viridis(n=3)[1])
+rose.diag(Theta[251:300], bins=8, main="Phase IV", prop=2, col=viridis(n=3)[3])
+rose.diag(Theta[301:400], bins=8, main="Phase V", prop=2, col=viridis(n=3)[1])
+par(mfrow=c(1,1))
+
+
+### Testing for modification of shape and scale params of Weibull distribution
+# set.seed(1)
+# test<- BCRW(n = 100, rho=0.9, attraction = 0.9, a=1, b=30, Z.center = 20)
+# dist<- sqrt((test$X)^2 + (test$Y)^2)
+# S<- diff(dist)
+# plot(test)
+# plot.ts(S); mean(S)
+# 
+# 
+# #Evaluate modifications of Weibull distrib (for SL)
+# W.5_1<- rweibull(500, 5, 1) %>% data.frame(value = ., type = "a=5,b=1")  #Resting/Encamped
+# W.1_2<- rweibull(500, 1, 2) %>% data.frame(value = ., type = "a=1,b=2")  #ARS/Exploratory
+# W.1_10<- rweibull(500, 1, 10) %>% data.frame(value = ., type = "a=1,b=10")  #Transit
+# 
+# W<- rbind(W.5_1, W.1_2, W.1_10)
+# ggplot(W, aes(x=value)) +
+#   geom_density(aes(fill = type), alpha = 0.6)
+
+
+
+
+
+
+
+### My Simulation: Only for identifying behaviors ###
+## Uses multistate CVM model
+set.seed(10)
+nus <- c(1,2.5,1,10,1)
+taus <- c(1,1,1,10,1)
+Ts <- c(100,50,100,50,100)
+CVM.sim <- multiCVM(taus, nus, Ts)
+plot(CVM.sim, col = viridis(n=3)[c(1,2,1,3,1)])
+
+#calculate and plot mean speeds
+S <- diff(CVM.sim$Z) %>% Mod()
+
+nu.means<- matrix(NA, 5, 1)
+for (i in 1:length(unique(CVM.sim$Phase))) {
+  ind<- which(CVM.sim$Phase == i)
+  
+  nu.means[i,]<- mean(CVM.sim$Z[ind] %>% diff() %>% Mod(), na.rm = T)
+}
+
+
+plot.ts(S)
+abline(v = c(100-1,150-2,250-3,300-4), lwd=3, col="darkgrey", lty=3)
+clip(1,99,0,20)
+abline(h=nu.means[1], col="red", lwd=2)
+clip(100,148,0,20)
+abline(h=nu.means[2], col="red", lwd=2)
+clip(149,247,0,20)
+abline(h=nu.means[3], col="red", lwd=2)
+clip(248,296,0,20)
+abline(h=nu.means[4], col="red", lwd=2)
+clip(297,395,0,20)
+abline(h=nu.means[5], col="red", lwd=2)
+
+
+
+
+# absolute orientation
+Phi <- diff(CVM.sim$Z) %>% Arg()
+# turning angles
+Theta <- diff(Phi)
+# rose diagrams
+require(circular)
+par(mfrow=c(1,5))
+rose.diag(Theta[1:100], bins=8, main="Phase I", prop=2, col=viridis(n=3)[1])
+rose.diag(Theta[101:150],bins=8, main="Phase II", prop=2, col=viridis(n=3)[2])
+rose.diag(Theta[151:250], bins=8, main="Phase III", prop=2, col=viridis(n=3)[1])
+rose.diag(Theta[251:300], bins=8, main="Phase IV", prop=2, col=viridis(n=3)[3])
+rose.diag(Theta[301:400], bins=8, main="Phase V", prop=2, col=viridis(n=3)[1])
+par(mfrow=c(1,1))
