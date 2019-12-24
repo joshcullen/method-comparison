8cfc547b62f8f7fc8fc74ecd0bff2d438de22652
Josh Cullen <joshcullen@Joshs-MacBook-Pro-3.local>
2019-12-24 15:36:57 -0500

    Include lakes in map

--- Compare KDE vs ACs.R
+++ Compare KDE vs ACs.R
@@ -5,6 +5,9 @@ library(adehabitatHR)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(viridis)
+library(ks)
+library(raster)
+library(sf)
 
 
 dat<- read.csv("Snail Kite Gridded Data_larger.csv", as.is = TRUE)
@@ -23,19 +26,26 @@ usa <- ne_states(country = "United States of America", returnclass = "sf")
 fl<- usa %>% filter(name == "Florida")
 fl<- sf::st_transform(fl, crs = "+init=epsg:32617") #change projection to UTM 17N
 
+# lakes
+lakes10 <- ne_download(scale = 10, type = 'lakes', category = 'physical', returnclass = "sf")
+lakes10<- sf::st_transform(lakes10, crs = "+init=epsg:32617") %>%
+  sf::st_crop(xmin = min(dat$x-20000), xmax = max(dat$x+20000), ymin = min(dat$y-20000),
+              ymax = max(dat$y+20000))
+
 nests<- dat %>% group_by(id) %>% dplyr::select(c(id, x, y)) %>% slice(n=1)
 
 
 # ACs
 ggplot() +
   geom_sf(data = fl) +
+  geom_sf(data = lakes10, fill = "lightblue", alpha = 0.65) +
   coord_sf(xlim = c(min(dat$x-20000), max(dat$x+20000)),
            ylim = c(min(dat$y-20000), max(dat$y+20000)), expand = FALSE) +
   geom_point(data = dat, aes(x, y, color = "Raw"), size = 1, alpha = 0.5) +
   geom_point(data = ac.coords, aes(x, y, color = "Model"), size = 3) +
   geom_point(data = nests, aes(x, y, color = "Nests"), shape = 17, size = 2) +
   labs(x="Longitude", y="Latitude") +
-  scale_color_manual("", values = c(viridis(n=5)[3],"red","grey45")) +
+  scale_color_manual("", values = c(viridis(n=3)[1],"red","grey45")) +
   guides(color = guide_legend(override.aes = list(shape = c(16,17,16)))) +
   theme_bw()
 
